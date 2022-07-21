c2649550b1077644443dc8560d3f43af20a4a037
dc <s10843047@ems.tcust.edu.tw>
2022-07-21 17:48:00 +0800

    0720 1.optimize updateProductInfo() 2.Test Abort message

--- application/controllers/product.py
+++ application/controllers/product.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
 from flask_restful import Resource, reqparse
-from db import db
 from ..models.product import Product
+from flask import abort
+import traceback,sys
+def abort_msg(e):
+    """500 bad request for exception
 
+    Returns:
+        500 and msg which caused problems
+    """
+    error_class = e.__class__.__name__ # 引發錯誤的 class
+    detail = e.args[0] # 得到詳細的訊息
+    cl, exc, tb = sys.exc_info() # 得到錯誤的完整資訊 Call Stack
+    lastCallStack = traceback.extract_tb(tb)[-1] # 取得最後一行的錯誤訊息
+    fileName = lastCallStack[0] # 錯誤的檔案位置名稱
+    lineNum = lastCallStack[1] # 錯誤行數 
+    funcName = lastCallStack[2] # function 名稱
+    # generate the error message
+    errMsg = "Exception raise in file: {}, line {}, in {}: [{}] {}. Please contact the member who is the person in charge of project!".format(fileName, lineNum, funcName, error_class, detail)
+    # return 500 code
+    abort(500, errMsg)
 class ProductController(Resource):
 
     LIST_URL= '/product/<product_id>'
     CREATE_URL= '/product/'
-    parser = reqparse.RequestParser()
-    parser.add_argument('product_name', required=True, help='Product Name is required',location=['form'])
-    parser.add_argument('product_model', required=True, help='Product Model is required',location=['form'])
+
     
     #用產品id做查詢
     def get(self, product_id):
@@ -17,9 +32,8 @@ class ProductController(Resource):
             product_info_json = Product.getProductById(product_id)
             return product_info_json
         except Exception as e:
-                print (e)
+                abort_msg(e)
                 return {"error message":"無法查詢該筆資料,請檢查是否輸入錯誤"}
-
     # 新增資料
     def post(self):
         return {"message":'請至/addproduct 新增產品'}
@@ -28,31 +42,25 @@ class ProductController(Resource):
     def put(self):
         try:
             parser = reqparse.RequestParser()
-            parser.add_argument('product_id', required=True, help='Product id is required',location=['form'])
-            parser.add_argument('product_name', required=True, help='Product Name is required',location=['form'])
-            parser.add_argument('product_model', required=True, help='Product Model is required',location=['form'])
-            parser.add_argument('brand_id', help='Product Model is required',location=['form'])
-            parser.add_argument('category_id',help='Product Model is required',location=['form'])
-            parser.add_argument('supplier_id', help='Product Model is required',location=['form'])
-            parser.add_argument('details', required=False, help='details is required',location=['form'])
+            parser.add_argument('product_id', required=True, help='Product id is required')
+            parser.add_argument('product_name', help='Product Name is required')
+            parser.add_argument('product_model',help='Product Model is required')
+            parser.add_argument('brand_id', help='brand_id is required')
+            parser.add_argument('category_id',help='category_id is required')
+            parser.add_argument('supplier_id', help='supplier_id is required')
+            parser.add_argument('details', required=False, help='details is required')
             req_data =parser.parse_args()
         except Exception as e:
-            return {"error message":f'{e}'}
+            return {"error message":f'{e}可能參數不齊全'}
         else:
-            update_list = []
-            for data in req_data.items():
-                if data[1] != None :
-                    update_list.append(data)
-
-            updateMessage = Product.UpdateProductInfo(req_data['product_id'],update_list)
-
+            updateMessage = Product.UpdateProductInfo(req_data['product_id'],req_data)
             return {"message":f"{updateMessage}"}
 
     # 刪除資料by product_id
     def delete(self):
         try:
             parser = reqparse.RequestParser()
-            parser.add_argument('product_id', required=True, help='Product id is required',location=['form'])
+            parser.add_argument('product_id', required=True, help='Product id is required')
             req_data =parser.parse_args()
             returnMessage = Product.DeleteProduct(req_data['product_id'])
             return returnMessage
@@ -65,8 +73,8 @@ class ProductController(Resource):
 class ProductsListController(Resource):
     LIST_URL='/products/'
     parser = reqparse.RequestParser()
-    parser.add_argument('product_name', required=True, help='Product Name is required',location=['form'])
-    parser.add_argument('product_model', required=True, help='Product Model is required',location=['form'])
+    parser.add_argument('product_name', required=True, help='Product Name is required')
+    parser.add_argument('product_model', required=True, help='Product Model is required')
     
     # 取得所有產品記錄
     def get(self):
@@ -77,12 +85,12 @@ class AddProductController(Resource):
 
     LIST_URL= '/addproduct/'
     parser = reqparse.RequestParser()
-    parser.add_argument('product_name', required=True, help='Product Name is required',location=['form'])
-    parser.add_argument('product_model', required=True, help='Product Model is required',location=['form'])
-    parser.add_argument('brand_id', required=True, help='brand_id is required',location=['form'])
-    parser.add_argument('category_id', required=True, help='category_id is required',location=['form'])
-    parser.add_argument('supplier_id', required=True, help='supplier_id is required',location=['form'])
-    parser.add_argument('details', required=True, help='details is required',location=['form'])
+    parser.add_argument('product_name', required=True, help='Product Name is required')
+    parser.add_argument('product_model', required=True, help='Product Model is required')
+    parser.add_argument('brand_id', required=True, help='brand_id is required')
+    parser.add_argument('category_id', required=True, help='category_id is required')
+    parser.add_argument('supplier_id', required=True, help='supplier_id is required')
+    parser.add_argument('details', required=True, help='details is required')
     
     #取得新增產品所需資料
     def get(self):
